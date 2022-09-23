a8f3d02b071f240e446f4e66adb675598556daf7
Rodrigo <maceno@gmx.com>
2022-09-23 15:48:19 -0600

    .

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Person from './person'
+// import React from "react";
+
+// export default class Person extends React.Component {
+//     render(){
+//         return(
+//             <div>Hi</div>
+//         )
+//     }
+// }
+
+
+
+let person = new Person('Sam', 'Smith');
+
+person.display('myDiv')
+
+
